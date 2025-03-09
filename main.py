acba5618a048004ef6b554abf53ba8937e32b481
OrBiT-qwr <vania032003@gmail.com>
2025-03-09 20:21:46 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+
+import telebot
+from model import get_class
+
+
+# Замени 'TOKEN' на токен твоего бота
+bot = telebot.TeleBot("your token")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin  ")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
+    file_name = file_info.file_path.split('/')[-1]
+    downloaded_file = bot.download_file(file_info.file_path) 
+    with open(file_name, 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    result = get_class(model_path='keras_model.h5',labels_path='labels.txt',image_path=file_name)
+    bot.send_message(message.chat.id,result )
+
+# Запускаем бота
+bot.polling()
